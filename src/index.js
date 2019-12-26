a45d357ec0fb8b1e470fbc97b7cb8ea8779b252f
Waldek <waldek.pieniak@gmail.com>
2019-12-26 14:17:15 +0100

    Api restored

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const express = require('express');
 //
 // const PORT = process.env.PORT || 4000;
 //
-// const app = express();
+const app = express();
 //
 // app.use(cors());
 //
-// app.use('/', (req,res) => res.send('<h1>To jest proba</h1>'))
-//
-// app.listen(2000, () => console.log('Listening at port', 2000))
 
 console.log('bbbbb')
+
+app.use('/', (req,res) => res.send('<h1>To jest proba</h1>'))
+//
+app.listen(2000, () => console.log('Listening at port', 2000))
