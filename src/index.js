a9d24b0ca5cbc61bb69eb651ee16ba26fc7392f1
Waldek <waldek.pieniak@gmail.com>
2019-12-26 13:46:21 +0100

    Basic express api setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-console.log('Test', process.env.MY_SECRET)
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req,res) => res.send('<h1>To jest proba</h1>'))
+
+app.listen(PORT, () => console.log('Listening at port', PORT))
