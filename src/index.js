8c09f48e5f5de177a1391e55653cc82aaa574a2c
Waldek <waldek.pieniak@gmail.com>
2019-12-26 13:55:29 +0100

    Babel deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 
