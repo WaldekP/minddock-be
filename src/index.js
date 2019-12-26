1b1c52212a13af7e842e04f2493ef68f5b8ba25a
Waldek <waldek.pieniak@gmail.com>
2019-12-26 14:11:46 +0100

    Tem fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express');
-const cors = require('cors');
-
-const PORT = process.env.PORT || 4000;
-
+// const cors = require('cors');
+//
+// const PORT = process.env.PORT || 4000;
+//
 const app = express();
+//
+// app.use(cors());
+//
+app.use('/', (req,res) => res.send('<h1>To jest proba</h1>'))
+//
+app.listen(2000, () => console.log('Listening at port', 2000))
 
-app.use(cors());
-
-app.get('/', (req,res) => res.send('<h1>To jest proba</h1>'))
-
-app.listen(PORT, () => console.log('Listening at port', PORT))
+console.log('dssdds')
