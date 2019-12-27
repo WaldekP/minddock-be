53b1b0f46732ecaf10e9ec2fc7abf341964bf6a7
Waldek <waldek.pieniak@gmail.com>
2019-12-27 20:30:36 +0100

    db url moved to .env file

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const router = require('../routes/index');
 const mongoose = require('mongoose');
-const Psychologist = require('../models/psychologist')
-const foodRouter = require('../models/food')
-
-// const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://waldek:azorek1@cluster0-ipbva.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-mongoose.connect(uri, {useNewUrlParser: true}).catch(e => console.log('BLA', e))
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true }).catch(e => console.log('BLA', e))
 
 app.use(router.psychologists)
 //
