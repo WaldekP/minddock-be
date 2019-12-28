cd4c031a65e4532bc62b41412f236288b8a43555
Waldek <waldek.pieniak@gmail.com>
2019-12-28 15:37:30 +0100

    Temp changes

--- routes/psychologists.js
+++ routes/psychologists.js
@@ -19,5 +19,11 @@ router.post('/', async (req, res) => {
     await psychologist.save();
     return res.status(200).send(req.body);
 });
+
+router.put('/:id', async (req, res) => {
+    console.log('query', req.query)
+    const psychologist = await psychologistModel.findByIdAndUpdate({_id: "5e05ef91084a7c2e01e32aa7"}, {name: req.query.name}, { upsert: true },)
+    res.status(200).send(psychologist)
+})
 module.exports = router;
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 const express = require('express');
 const router = require('../routes/index');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true }).catch(e => console.log('BLA', e))
-
-app.use(router.psychologists)
+// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true }).catch(e => console.log('BLA', e))
+app.get('/', (req,res) => res.send('<h1>Co to sie zadzialo</h1>'))
+// app.use('/psychologists', router.psychologists)
 //
 app.listen(process.env.PORT || 2000, () => console.log('Listening at port', 2000))
