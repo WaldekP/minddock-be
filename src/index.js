9a60dbdf80cd39a2c60552c4a849254798f175cb
Waldek <waldek.pieniak@gmail.com>
2019-12-28 17:05:31 +0100

    route path fixed

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', (req,res) => res.send('<h1>Moze zadziala</h1>'))
 app.use('/psychologists', router.psychologists)
 //
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true }).then((result)  => app.listen(process.env.PORT || 2000, () => console.log('Server running on port', 2000))).catch(e => console.log('BLA', e))
