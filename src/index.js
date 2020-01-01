cb293c095149ebe33214ebc9905c8b6c2142e010
Waldek <waldek.pieniak@gmail.com>
2020-01-01 19:34:46 +0100

    cors origin changed

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 // app.use(cors(corsOptions));
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://minddock-front-66zxckuls.now.sh/");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', true)
