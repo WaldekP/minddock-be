5ed06253da18ec4006d5d0b8dae6626e8df72be9
Waldek <waldek.pieniak@gmail.com>
2020-01-01 18:37:10 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -23,8 +23,6 @@ app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, s
         sameSite: 'lax',
         secure: false,
         httpOnly: false,
-        domain: 'http://localhost:3000',
-        path:'/profile'
     }}));
 
 app.get('/', (req,res) => res.send('<h1>Moze zadziala</h1>'))
